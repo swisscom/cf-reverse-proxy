5aa58d9ea4823201f4fcb636e93f73ac7a7ae389
Tobias Fuhrimann <tobias.fuhrimann@swisscom.com>
2016-11-30 11:16:12 +0100

    Use winston for logging

--- src/server.js
+++ src/server.js
@@ -3,26 +3,20 @@
 const url = require("url");
 const http = require("http");
 const httpProxy = require("http-proxy");
+const winston = require("winston");
 
 const vcapServices = JSON.parse(process.env.VCAP_SERVICES);
 const kibanaUrl = url.parse(vcapServices.elk[0].credentials.kibanaUrl);
-
-const addresses = [
-  {
-    host: kibanaUrl.hostname,
-    port: kibanaUrl.port,
-  },
-];
+const address = {
+  host: kibanaUrl.hostname,
+  port: kibanaUrl.port,
+};
 
 const proxy = httpProxy.createServer();
 
 const server = http.createServer((req, res) => {
-  const target = { target: addresses.shift() };
-
-  console.log("Proxy request to:", target);
-  proxy.web(req, res, target);
-
-  addresses.push(target.target);
+  winston.info("Proxy request to:", address);
+  proxy.web(req, res, { target: address });
 });
 
 module.exports = server;
